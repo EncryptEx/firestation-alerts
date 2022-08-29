a754b84ab64513dd72a95063900420724dd36606
EncryptEx <limusina10@gmail.com>
2022-08-29 23:53:25 +0200

    optimized alerts

--- src/private/alerts/Alerts.php
+++ src/private/alerts/Alerts.php
@@ -1,33 +1,58 @@
-<?php 
+<?php
 
 namespace Utils;
 
-class Alerts {
-    public function retrieveAlert(){
-        if(isset($_GET['e'])){
-            if(!is_numeric($_GET['e'])) {return;} // skip check if not numeric
-            switch ($_GET['e']) {
-                case 1:
-                    $error = "User and password do not match";
-                    break;
+class Alerts
+{
+    public function retrieveAlert()
+    {
+        if (isset($_GET['e']) || isset($_GET['s'])) {
+            if (isset($_GET['e'])) {
+                if (!is_numeric($_GET['e'])) {
+                    return;
+                } // skip check if not numeric
+                switch ($_GET['e']) {
+                    case 1:
+                        $alertMsg = "User and password do not match";
+                        break;
 
-                default:
-                    $error = "Something did not work as expected";
-                    break;
+                    default:
+                        $alertMsg = "Something did not work as expected";
+                        break;
                 }
-                $errorTemplate = "<div class='alert alert-danger d-flex align-items-center alert-dismissible fade show' role='alert'>
-                <span class='material-symbols-outlined align-middle' style='margin-right:10px'>
-                    warning
-                </span>
-                <span class='align-middle'>
-                    " . $error . "
-                </span>
-                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' onclick=\"window.location.href = window.location.href.split('?')[0];\"></button>
-                </div>";
-                echo $errorTemplate;
-            
-        } else if (isset($_GET['s'])){
+                
+                $className = "danger"; // bs5 red alert
+                $icon = "warning"; // bs5 icon alert
+                
+            } else if (isset($_GET['s'])) {
+                if (!is_numeric($_GET['s'])) {
+                    return;
+                } // skip check if not numeric
+                switch ($_GET['s']) {
+                    case 1:
+                        $alertMsg = "";
+                        break;
 
+                    default:
+                        $alertMsg = "Something did not work as expected";
+                        break;
+                }
+
+                $className = "success"; // bs5 green alert
+                $icon = "check_circle"; // bs5 icon alert
+            }
+            
+            // common action in both cases
+            $alertTemplate = "<div class='alert alert-" . $className . " d-flex align-items-center alert-dismissible fade show' role='alert'>
+            <span class='material-symbols-outlined align-middle' style='margin-right:10px'>
+                " . $icon . "
+            </span>
+            <span class='align-middle'>
+                " . $alertMsg . "
+            </span>
+            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' onclick=\"window.location.href = window.location.href.split('?')[0];\"></button>
+            </div>";
+            echo $alertTemplate;
         }
     }
 }
