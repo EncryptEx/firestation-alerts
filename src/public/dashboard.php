ca77943be84785e0d4d41d00cee626f106a8f986
EncryptEx <limusina10@gmail.com>
2022-09-10 22:08:32 +0200

    added backend in dashboard

--- src/public/dashboard.php
+++ src/public/dashboard.php
@@ -1,10 +1,40 @@
+<?php
+session_start();
+
+use Utils\Auth;
+
+require './../private/utils.php';
+// protected-view file
+$auth = new Auth;
+$auth->checkUserLogged($_SESSION['userId']);
+
+use Utils\User;
+
+$user = new User;
+$userInfo = $user->GetInfo($_SESSION['userId']);
+
+use Utils\CountryHelper;
+
+$country = new CountryHelper;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php require realpath(__DIR__ . '/../private/templates/meta.html'); ?>
 </head>
+
 <body>
-    <?php require realpath(__DIR__ . '/../private/templates/navbar.php');?>
+    <?php require realpath(__DIR__ . '/../private/templates/navbar.php'); ?>
     <!-- dashboard goes here -->
+    <div class="container">
+        <br>
+        <h1>Watching all alerts in <?php echo $country->GetCountryName($userInfo['countryCode']); ?></h1>
+        <?php
+        ?>
+
+    </div>
 </body>
+
 </html>
